3f58801f36015271a6397f556a5eec0f91444ada
bikram294 <bikram.singh294@gmail.com>
2022-03-14 22:23:55 +0530

    home completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
+import React from 'react';
 import './App.css';
 import FirstPage from './Components/FirstPage';
+import Home from './Components/Home';
+import {useState} from "react";
+
+
 
 function App() {
+  const[visible, setVisisble]=useState(true);
+  const shouldRender=()=>{
+    setVisisble(false);
+  };
+
+
   return (
     <div>
-      <FirstPage/>
+      {visible && <FirstPage shouldRender={shouldRender}/>}
+      {!visible && <Home/>}
     </div>
   );
 }
--- src/Components/FirstPage.jsx
+++ src/Components/FirstPage.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import image from '../scholarsiportal.png';
 import './FirstPage.css';
 
-const FirstPage=()=>{
+
+
+const FirstPage=(props)=>{
+    const clickHandler=()=>{
+        props.shouldRender();
+    }
+
     return <React.Fragment>
         <div className="bgimg">
             <img src={image} alt='' />
         </div>
         <div className="footer">
-            <button className="btn" type="submit">Enter Site!</button>
+            <button className="btn" onClick={clickHandler}>Enter Site!</button>
         </div>
     </React.Fragment>
 };
